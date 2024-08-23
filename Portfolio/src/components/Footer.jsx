4996f9d8aa8e8c3252a45d9768f1207e443e22ca
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-08-23 20:12:52 +0530

    Footer.jsx File updated import logo typo error resolved

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logo.png'
+import Logo from '../assets/Logo.png'
 import { SOCIAL_MEDIA_LINKS } from '../constants';
 import { motion } from 'framer-motion';
 
